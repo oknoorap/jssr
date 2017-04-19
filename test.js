43ae381cf543f6ffcac2ed840d5f4e91bc56eeea
oknoorap <oknoorap@gmail.com>
2017-04-19 14:41:09 +0700

    fix: better test architecture

--- test.js
+++ test.js
@@ -2,39 +2,36 @@ import test from 'ava'
 import deepEqual from 'deep-equal'
 import jssr from './index'
 
-class stdClass {
-  constructor (obj) {
-    return obj
-  }
-}
-
 const str = 'hello world'
-const strSerialize = 's:11:"hello world";'
+const strSerialized = 's:11:"hello world";'
 
 const int = 10
 const intSerialized = 'i:10;'
 
 const bool = true
-const boolSerialized = 'b:true;'
+const boolSerialized = 'b:1;'
 
 const array = ['strArray', 10, false, {key: 'value'}]
 const arrSerialized = 'a:4:{i:0;s:8:"strArray";i:1;i:10;i:2;b:0;i:3;a:1:{s:3:"key";s:5:"value";}}'
 
 const obj = {
   greetings: 'hello',
-  obj: new stdClass({
+  customObject: jssr.objType('stdClass', {
     str: 'universe',
     good: true
-  })
+  }),
+  normalObject: {
+    good: true
+  }
 }
-const objSerialized = 'a:2:{s:9:"greetings";s:5:"hello";s:3:"obj";O:8:"stdClass":2:{s:3:"str";s:8:"universe";s:4:"good";b:1;}}'
+const objSerialized = 'a:3:{s:9:"greetings";s:5:"hello";s:12:"customObject";O:8:"stdClass":2:{s:3:"str";s:8:"universe";s:4:"good";b:1;}s:12:"normalObject";a:1:{s:4:"good";b:1;}}'
 
 test('string should be serialized', t => {
-  t.is(jssr.stringify(str), strSerialize)
+  t.is(jssr.stringify(str), strSerialized)
 })
 
 test('serialized string should be string', t => {
-  t.is(str, jssr.parse(strSerialize))
+  t.is(str, jssr.parse(strSerialized))
 })
 
 test('number should be serialized', t => {
@@ -62,7 +59,8 @@ test('serialized array should be array', t => {
 })
 
 test('object should be serialized', t => {
-  t.is(jssr.stringify(obj, objSerialized))
+  const object = jssr.stringify(obj)
+  t.is(object, objSerialized)
 })
 
 test('serialized object should be object', t => {
